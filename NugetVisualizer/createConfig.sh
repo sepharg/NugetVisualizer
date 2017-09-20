925cf93be029b3a93da6a0a5ea1a3c1dee666860
MOONPIG\daniel.perez <daniel.perez@moonpig.com>
2017-09-20 15:17:50 +0100

    adding script that will be in charge of creating configuration files for build server

---
+++ NugetVisualizer/createConfig.sh
@@ -0,0 +1,10 @@
+echo "{
+  \"GithubOrganization\": \"ORG\",
+  \"GithubToken\": \"TOKEN\",
+  \"Dbpath\": \"C:\\Development\\NugetVisualizer\\NugetVisualizer\\Core\\nugetvisualizerdb\"
+}" > ConsoleVisualizer\configuration.json
+
+echo "{
+  \"GithubOrganization\": \"ORG\",
+  \"GithubToken\": \"TOKEN\"
+}" > UnitTests\configuration.json
