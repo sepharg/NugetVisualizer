3fd4cac3f56775f00e8e3987fc8826c5364dc14f
MOONPIG\daniel.perez <daniel.perez@moonpig.com>
2017-09-20 15:51:49 +0100

    fixing token for tests

--- NugetVisualizer/createConfig.sh
+++ NugetVisualizer/createConfig.sh
@@ -1,10 +1,10 @@
 echo "{
-  \"GithubOrganization\": \"ORG\",
-  \"GithubToken\": \"TOKEN\",
+  \"GithubOrganization\": \"sepharg\",
+  \"GithubToken\": \"aa0746e8a4599c916e6e89560e0f01dad5fa4eb2\",
   \"Dbpath\": \"C:\\Development\\NugetVisualizer\\NugetVisualizer\\Core\\nugetvisualizerdb\"
 }" > ConsoleVisualizer\\configuration.json
 
 echo "{
-  \"GithubOrganization\": \"ORG\",
-  \"GithubToken\": \"TOKEN\"
+  \"GithubOrganization\": \"sepharg\",
+  \"GithubToken\": \"aa0746e8a4599c916e6e89560e0f01dad5fa4eb2\"
 }" > UnitTests\\configuration.json
