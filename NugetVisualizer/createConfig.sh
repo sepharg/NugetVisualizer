080d9b93b8209ee8a6093fcccf5bc56e8867db5e
MOONPIG\daniel.perez <daniel.perez@moonpig.com>
2017-09-21 10:09:38 +0100

    add token to env variable

--- NugetVisualizer/createConfig.sh
+++ NugetVisualizer/createConfig.sh
@@ -1,10 +1,10 @@
 echo "{
   \"GithubOrganization\": \"Sephargorganization\",
-  \"GithubToken\": \"a320c2251732f5d3db06b3c9378a3cd2d4b0ed54\",
+  \"GithubToken\": \"$1\",
   \"Dbpath\": \"C:\\Development\\NugetVisualizer\\NugetVisualizer\\Core\\nugetvisualizerdb\"
 }" > ConsoleVisualizer\\configuration.json
 
 echo "{
   \"GithubOrganization\": \"Sephargorganization\",
-  \"GithubToken\": \"a320c2251732f5d3db06b3c9378a3cd2d4b0ed54\"
+  \"GithubToken\": \"$1\"
 }" > UnitTests\\configuration.json
