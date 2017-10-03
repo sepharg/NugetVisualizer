7550399995038ce6ca05e5a0ffa286b6bd7feaf6
Daniel Perez <danielperez.arg@gmail.com>
2017-10-03 10:55:01 +0100

    added simple dashboard skeleton (css and js inline, will need to tidy up after)

---
+++ NugetVisualizer/WebVisualizer/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebVisualizer.Controllers
+{
+    using WebVisualizer.Models;
+
+    public class DashboardController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var mostUsedPackagesViewModel = new MostUsedPackagesViewModel();
+            mostUsedPackagesViewModel.SetPackageList(new[] { "uno", "dos", "tres" });
+            mostUsedPackagesViewModel.SetUsagesValues(new [] { 33, 12, 20 });
+            var model = new DashboardViewModel() { MostUsedPackagesViewModel = mostUsedPackagesViewModel };
+
+            return View(model);
+        }
+    }
+}
---
+++ NugetVisualizer/WebVisualizer/Models/DashboardViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace WebVisualizer.Models
+{
+    using System.Linq;
+
+    public class MostUsedPackagesViewModel
+    {
+        private string _packageList;
+
+        private string _usagesValues;
+
+        public void SetPackageList(string[] packageNames)
+        {
+            _packageList = string.Join(',', packageNames.Select(name => $"\"{name}\""));
+        }
+
+        public void SetUsagesValues(int[] packageUsages)
+        {
+            _usagesValues = string.Join(',', packageUsages.Select(usage => usage.ToString()));
+        }
+
+        public string PackageList => _packageList;
+
+        public string UsagesValues => _usagesValues;
+    }
+
+    public class DashboardViewModel
+    {
+        public MostUsedPackagesViewModel MostUsedPackagesViewModel { get; set; }
+    }
+}
